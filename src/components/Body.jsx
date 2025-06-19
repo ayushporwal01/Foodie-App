dce551eadf7fe4d1147c6f8abfafb2549f78b0d3
ayushporwal01 <porwalayush953@gmail.com>
2025-06-19 21:21:14 +0530

    episode-6

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   //Local State Variable - Superpowerful React Variable
-  const [ListOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [ListOfRestaurants, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=22.7195687&lng=75.8577258&carousel=true&third_party_vendor=1"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (ListOfRestaurants.length === 0) {
+    return (
+      <Shimmer />
+    );
+  }
 
   return (
     <div className="body">
