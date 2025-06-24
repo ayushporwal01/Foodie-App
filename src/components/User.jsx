1cb7be514393d4ae8b16c4e0d5cc222b95701608
ayushporwal01 <porwalayush953@gmail.com>
2025-06-24 17:35:46 +0530

    episode-7

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
 const User = ({ name, location }) => {
-  const count = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div className="user-card">
       <h1>Count = {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase Count
+      </button>
       <h2>Name: {name}</h2>
       <h3>Location: {location}</h3>
       <h4>Contact: @ayushporwal1</h4>
