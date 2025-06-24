608ff68644f531d9980f8e89d8dad0f6889a3908
ayushporwal01 <porwalayush953@gmail.com>
2025-06-24 17:50:32 +0530

    episode-7

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,8 @@ import { Component } from "react";
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log("Parent Constructor");
   }
   render() {
-    console.log("Parent Render");
     return (
       <div>
         <h1>About</h1>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -8,14 +8,10 @@ class UserClass extends React.Component {
       count: 0,
       //count2: 0
     };
-
-    console.log("Child Constructor");
   }
   render() {
     const { name, location } = this.props;
     const { count } = this.state;
-
-    console.log("Child Render");
 
     return (
       <div className="user-card">
