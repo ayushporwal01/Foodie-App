6e2055681a0c000f7c47e958c36dec444ecb7d0c
ayushporwal01 <porwalayush953@gmail.com>
2025-06-24 17:18:23 +0530

    episode-7

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,16 +6,14 @@ class UserClass extends React.Component {
 
     this.state = {
       count: 0,
-      count2: 2,
     };
   }
   render() {
     const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    const { count } = this.state;
     return (
       <div className="user-card">
         <h1>Count = {count}</h1>
-        <h1>Count2 = {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @ayushporwal1</h4>
