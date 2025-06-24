54a3bbc3e2dfe84fe9867daf8070667dba31ddd9
ayushporwal01 <porwalayush953@gmail.com>
2025-06-24 17:12:42 +0530

    episode-7

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is Episode 7 of Namaste React</h2>
+      <User name={"Ayush Porwal(function)"} location={"Indore Function"} />
+      <UserClass name={"Ayush Porwal(class)"} location={"Indore Class"} />
     </div>
   );
 };
---
+++ src/components/UserClass.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
+  }
+  render() {
+    const { name, location } = this.props;
+    const { count, count2 } = this.state;
+    return (
+      <div className="user-card">
+        <h1>Count = {count}</h1>
+        <h1>Count2 = {count2}</h1>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h4>Contact: @ayushporwal1</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
