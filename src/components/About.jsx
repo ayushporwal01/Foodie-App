bb18e873ddb10bc3bd126ef3c64787365c222c3b
ayushporwal01 <porwalayush953@gmail.com>
2025-06-24 17:17:25 +0530

    episode-7

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-import User from "./User";
 import UserClass from "./UserClass";
 
 const About = () => {
@@ -6,7 +5,6 @@ const About = () => {
     <div>
       <h1>About</h1>
       <h2>This is Episode 7 of Namaste React</h2>
-      <User name={"Ayush Porwal(function)"} location={"Indore Function"} />
       <UserClass name={"Ayush Porwal(class)"} location={"Indore Class"} />
     </div>
   );
