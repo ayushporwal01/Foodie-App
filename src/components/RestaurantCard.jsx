b805b6fcdbded97a0fba71ba92e51e907fd05254
ayushporwal01 <porwalayush953@gmail.com>
2025-06-28 17:28:25 +0530

    episode-11

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -23,4 +23,16 @@ const RestaurantCard = (props) => {
   );
 };
 
+//PromotedRestaurantCard
+export const withPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <label>Promoted</label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
+};
+
 export default RestaurantCard;
