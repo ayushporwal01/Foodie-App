e2fa361d96be67c9dd4fb0b4615f9e1044674995
ayushporwal01 <porwalayush953@gmail.com>
2025-06-30 08:36:34 +0530

    episode-11

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -21,14 +21,11 @@ const ItemsList = ({ items }) => {
               {item?.card?.info?.description}
             </p>
           </div>
-
-          {item?.card?.info?.imageId && (
-            <img
-              src={CDN_URL + item.card.info.imageId}
-              className="w-36 h-36 mb-2 rounded-xl"
-              alt={item?.card?.info?.name}
-            />
-          )}
+          <img
+            src={CDN_URL + item.card.info.imageId}
+            className="w-36 h-36 mb-2 rounded-xl"
+            alt={item?.card?.info?.name}
+          />
           <button className="w-28 h-10 text-lg font-bold uppercase border border-gray-400 rounded-[8px] text-green-600 bg-white shadow-[0px_0px_2px_2px_rgba(0,0,0,0.1)]  hover:bg-gray-200 transition-all duration-200 ease-in-out cursor-pointer">
             Add
           </button>
