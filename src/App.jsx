4b97f1c25696be848effa5fb30ed920aac15200f
ayushporwal01 <porwalayush953@gmail.com>
2025-06-30 19:16:47 +0530

    episode-11

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,9 @@ function AppLayout() {
     setUsername(data.name);
   }, []);
   return (
-    //Default
     <UserContext.Provider value={{ loggedInUser: username }}>
-      {/* Ayush Porwal */}
       <div className="app">
         <UserContext.Provider value={{ loggedInUser: "Elon Musk" }}>
-          {/* Elon Musk */}
           <Header />
         </UserContext.Provider>
         <main className="mt-24">
