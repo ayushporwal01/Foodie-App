0ae1c1e180cb48d8cc3a83312e0511e8656e71fc
ayushporwal01 <porwalayush953@gmail.com>
2025-06-30 09:01:11 +0530

    episode-11

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,12 +1,12 @@
 import ItemsList from "./itemList";
 
 const RestaurantCategory = ({ data }) => {
-  console.log(data);
+  const handleClick = () => {}
 
   return (
     <div>
       {/* Accordion Header */}
-      <div className="w-1/2 p-4 mx-auto my-4 bg-gray-200 shadow-lg">
+      <div className="w-1/2 p-4 mx-auto my-4 bg-gray-200 shadow-lg" onClick={handleClick}>
         <div className="flex justify-between">
           <span className="text-lg font-bold">
             {data.title}({data.itemCards.length})
