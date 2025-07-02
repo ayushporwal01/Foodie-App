983d618ee711b57a543defe783110a4fd20539c0
ayushporwal01 <porwalayush953@gmail.com>
2025-07-02 18:51:16 +0530

    episode-12

--- src/utils/cartSlice.jsx
+++ src/utils/cartSlice.jsx
@@ -8,6 +8,7 @@ const cartSlice = createSlice({
 
   reducers: {
     addItem: (state, action) => {
+      //mutating the state 
       state.items.push(action.payload);
     },
     removeItem: (state) => {
