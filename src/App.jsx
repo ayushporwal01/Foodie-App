39ae458c861340273495cb1a42013f5df110a946
ayushporwal01 <porwalayush953@gmail.com>
2025-07-02 18:50:41 +0530

    episode-12

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function AppLayout() {
   const [username, setUsername] = useState("");
@@ -12,14 +14,16 @@ function AppLayout() {
     setUsername(data.name);
   }, []);
   return (
-    <UserContext.Provider value={{ loggedInUser: username, setUsername }}>
-      <div className="app">
-        <Header />
-        <main className="mt-24">
-          <Outlet />
-        </main>
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: username, setUsername }}>
+        <div className="app">
+          <Header />
+          <main className="mt-24">
+            <Outlet />
+          </main>
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 }
 
