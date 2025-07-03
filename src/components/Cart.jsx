11c954ac718408cde365e8dcc212b512da8f96bc
ayushporwal01 <porwalayush953@gmail.com>
2025-07-03 12:23:09 +0530

    episode-12

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartItems = useSelector((store) => store.cart.items);
+  
+  return (
+    <div className="text-center m-10 p-10">
+      <h1 className="text-2xl font-bold">Cart</h1>
+    </div>
+  );
 };
 
 export default Cart;
