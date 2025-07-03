1e1dc3037a9bc36a5f8c4d5150848d65ce33be99
ayushporwal01 <porwalayush953@gmail.com>
2025-07-03 16:33:41 +0530

    episode-12

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,14 +14,20 @@ const Cart = () => {
     <div className="text-center m-10 p-10 dark:bg-black">
       <h1 className="text-2xl font-bold dark:text-white">Cart</h1>
       <button
-        className="m-2 p-2 bg-black dark:bg-gray-200 rounded-lg cursor-pointer"
+        className="m-2 p-2 bg-black text-white dark:bg-gray-200 dark:text-black rounded-lg cursor-pointer"
         onClick={handleClearCart}
       >
         Clear Cart
       </button>
-      <div className="w-6/12 mx-auto bg-gray-200 p-5 my-4 shadow-lg">
-        <ItemsList items={cartItems} />
-      </div>
+      {cartItems.length > 0 ? (
+        <div className="md:w-6/12 mx-auto bg-gray-200 p-5 my-4 shadow-lg">
+          <ItemsList items={cartItems} />
+        </div>
+      ) : (
+        <div className="h-[calc(100vh-400px)] flex justify-center items-center">
+          <h1 className="text-lg">Your cart is empty, add items to cart!</h1>
+        </div>
+      )}
     </div>
   );
 };
