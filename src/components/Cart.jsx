a77439ca42e2d192b8cdde5909dafb21c1599c89
ayushporwal01 <porwalayush953@gmail.com>
2025-07-03 12:38:43 +0530

    episode-12

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,15 @@
 import { useSelector } from "react-redux";
+import ItemsList from "./itemList";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  
+
   return (
     <div className="text-center m-10 p-10">
       <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 mx-auto bg-gray-200 p-5 my-4 shadow-lg">
+        <ItemsList items={cartItems} />
+      </div>
     </div>
   );
 };
