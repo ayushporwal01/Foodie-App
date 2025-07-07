832ea0f6175b63f234780efede5dfe3faba49155
ayushporwal01 <porwalayush953@gmail.com>
2025-07-07 18:59:53 +0530

    episode-13

--- src/components/__test__/Header.test.js
+++ src/components/__test__/Header.test.js
@@ -2,11 +2,14 @@ import { render } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 test("should load button inside header component", () => {
   render(
-    <Provider store={appStore}>
-      <Header />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
   );
 });
