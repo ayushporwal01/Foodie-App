2be98ceff71270c901046aaefefc1a66a90a01f3
ayushporwal01 <porwalayush953@gmail.com>
2025-07-07 16:02:39 +0530

    episode-13

--- src/components/__test__/Contact.test.js
+++ src/components/__test__/Contact.test.js
@@ -1,11 +1,42 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 
-test("should load contact us component", () => {
-  render(<Contact />);
+describe("Test Cases for Contact Us Page", () => {
+  test("should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  //Assertion
-  expect(heading).toBeInTheDocument();
+    //Assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("should load button inside Contact component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    //Assertion
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should load input name inside Contact component", () => {
+    render(<Contact />);
+
+    //Querying
+    const inputName = screen.getByPlaceholderText("Your Name");
+
+    //Assertion
+    expect(inputName).toBeInTheDocument();
+  });
+
+  test("should load 3 input inside Contact component", () => {
+    render(<Contact />);
+
+    //Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    //Assertion
+    expect(inputBoxes.length).toBe(3);
+  });
 });
