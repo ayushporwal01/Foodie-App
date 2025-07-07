c6e7e9f5e375d175ea338330034445a7ddb350aa
ayushporwal01 <porwalayush953@gmail.com>
2025-07-07 18:53:46 +0530

    episode-13

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 
-
+// Add TextEncoder and TextDecoder for react-router-dom compatibility
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
