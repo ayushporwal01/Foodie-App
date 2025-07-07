aa2c947d6c63c041041f31ca72b330caacb636f1
ayushporwal01 <porwalayush953@gmail.com>
2025-07-07 22:26:29 +0530

    episode-13

--- src/components/__test__/RestaurantCard.test.js
+++ src/components/__test__/RestaurantCard.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../RestaurantCard";
 import { MOCK_DATA } from "../mocks/RestaurantMock";
 
-it("should render Restaurant Cards with Data", () => {
+it("should render Restaurant Cards with props Data", () => {
   render(<RestaurantCard resData={MOCK_DATA} />);
 
   const name = screen.getByText("Theobroma");
