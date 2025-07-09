41414b65f4ac4e434494cb435a66e4df2699c0a1
ayushporwal01 <porwalayush953@gmail.com>
2025-07-09 21:24:44 +0530

    episode-13

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -11,28 +11,28 @@ const RestaurantCard = (props) => {
   return (
     <div
       data-testid="resCard"
-      className="res-card w-70 rounded-lg bg-white text-black shadow-primary"
+      className="res-card rounded-lg bg-white text-black shadow-primary"
     >
       <img
-        className="res-logo w-full h-52 object-cover object-center p-3 rounded-2xl"
+        className="res-logo w-full h-44 object-cover p-3 rounded-2xl"
         src={CDN_URL + cloudinaryImageId}
       />
       <div className="res-info pl-4">
-        <h3 className="res-name font-bold text-gray-700 truncate">{name}</h3>
-        <h4 className="res-cuisine truncate text-sm text-gray-700">
+        <h3 className="res-name font-bold text-sm text-gray-700 truncate">{name}</h3>
+        <h4 className="res-cuisine truncate text-xs text-gray-700">
           {cuisines.join(", ")}
         </h4>
-        <h4 className="res-location text-sm text-gray-700">{areaName}</h4>
+        <h4 className="res-location text-xs text-gray-700">{areaName}</h4>
         <div className="space-x-2 flex items-center gap-1 py-2">
-          <span className="res-ratings px-1.5 py-0.5  bg-[#00ad1d] text-[14px] text-white font-semibold rounded flex items-center gap-1">
-            <i class="fa-solid fa-star text-[10px]"></i> {avgRating}
+          <span className="res-ratings px-1 py-0.5  bg-[#00ad1d] text-[12px] text-white font-semibold rounded flex items-center gap-1">
+            <i class="fa-solid fa-star text-[8px]"></i> {avgRating}
           </span>
-          <span className="text-gray-700 text-xl">•</span>
-          <span className="font-semibold text-gray-700 text-sm">
+          <span className="text-gray-700 text-lg">•</span>
+          <span className="font-semibold text-gray-700 text-[12px]">
             {lastMileTravelString}
           </span>
-          <span className="text-gray-700 text-xl">•</span>
-          <span className="font-bold text-gray-700 text-sm">{costForTwo}</span>
+          <span className="text-gray-700 text-lg">•</span>
+          <span className="font-bold text-gray-700 text-[12px]">{costForTwo}</span>
         </div>
       </div>
     </div>
