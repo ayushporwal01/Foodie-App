c824800c7475105cfd545a39a303fe1eb0741c40
ayushporwal01 <porwalayush953@gmail.com>
2025-07-09 17:58:03 +0530

    episode-13

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,45 +4,37 @@ const Contact = () => {
       className="flex justify-center items-center"
       style={{ minHeight: "calc(100vh - 96px)" }}
     >
-      <div className="w-[450px] h-[450px] border flex flex-col justify-center items-center space-y-10">
-        <h1 className="text-center font-bold text-3xl">Contact Us</h1>
+      <div className="w-[450px] h-[450px] flex flex-col justify-center items-center space-y-10">
+        <h1 className="text-center font-bold text-4xl text-gray-700">Contact Us</h1>
 
-        <form action="#" className="flex flex-col space-y-5">
-          <div className="flex space-x-3">
-            <input
-              type="text"
-              placeholder="Your Name"
-              required
-              className="border pl-2 ml-2"
-            />
-          </div>
-          <div>
-            <input
-              type="email"
-              placeholder="Your Email"
-              required
-              className="border pl-2 ml-2"
-            />
-          </div>
+        <form action="#" className="flex flex-col space-y-5 w-full">
+          <input
+            type="text"
+            placeholder="Your Name"
+            required
+            className="w-full border border-[#e08300] py-1.5 pl-2 rounded-lg focus:outline-none"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            required
+            className="w-full border border-[#e08300] py-1.5 pl-2 rounded-lg focus:outline-none"
+          />
 
-          <div className="flex flex-col">
-            <textarea
-              name="message"
-              rows="4"
-              placeholder="Your Message..."
-              required
-              className="w-58 border pl-2"
-            ></textarea>
-          </div>
+          <textarea
+            name="message"
+            rows="4"
+            placeholder="Your Message..."
+            required
+            className="w-full border border-[#e08300] py-5 pl-2 rounded-lg focus:outline-none"
+          ></textarea>
 
-          <div>
-            <button
-              type="submit"
-              className="w-full py-1 bg-[#ff9002] text-white rounded-sm transition-colors duration-300 cursor-pointer"
-            >
-              Submit
-            </button>
-          </div>
+          <button
+            type="submit"
+            className="w-20 py-1 bg-[#e08300] text-white font-medium rounded-md mx-auto transition-colors duration-300 hover:bg-[#ff9002]  cursor-pointer"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
