b77bdcefb70c3e3e474a7abfd5af3a20123c5d87
ayushporwal01 <porwalayush953@gmail.com>
2025-07-09 22:52:06 +0530

    episode-13

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,8 +1,7 @@
 const Shimmer = () => {
   return (
     <div className="flex justify-center items-center dark:bg-black">
-      <div className="shimmer-container grid grid-cols-5 gap-10 py-32">
-        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+      <div className="shimmer-container grid grid-cols-5 gap-10 py-6">
         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
