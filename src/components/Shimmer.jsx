3bad748802efd54339fb0bcd61495b65c8fe5117
ayushporwal01 <porwalayush953@gmail.com>
2025-07-10 18:24:49 +0530

    episode-13

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,47 +1,22 @@
-// const Shimmer = () => {
-//   return (
-//     <div className="flex justify-center items-center dark:bg-black">
-//       <div className="shimmer-container grid grid-cols-5 gap-10 py-6">
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//         <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-import { ShimmerPostItem } from "react-shimmer-effects";
-
 const Shimmer = () => {
   return (
     <div className="flex justify-center items-center dark:bg-black">
-      <div className="shimmer-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 py-6">
-        {/* Render shimmer cards dynamically */}
-        {Array.from({ length: 15 }).map((_, index) => (
-          <ShimmerPostItem
-            key={index}
-            card
-            title
-            text
-            cta
-            imageType="thumbnail"
-            imageWidth={80}
-            imageHeight={80}
-            contentCenter
-          />
-        ))}
+      <div className="shimmer-container grid grid-cols-5 gap-10 py-6">
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
+        <div className="w-56 h-64 bg-gray-200 dark:bg-gray-500"></div>
       </div>
     </div>
   );
