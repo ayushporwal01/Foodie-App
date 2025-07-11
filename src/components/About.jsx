8eaf5ec2946d4c6e3a15e012e5605edc3035ac20
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 09:54:20 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,15 @@ const [showProfile, setShowProfile] = useState(false);
 const About = () => {
   return (
     <div className="flex justify-center items-center py-48">
+      {/* Initial Content */}
       <div className="flex justify-center items-center gap-14">
         <h2 className="text-4xl font-bold text-gray-700">
           Home of your next favorite bite."
         </h2>
         <img src="/pizza.png" className="w-96 " alt="" />
       </div>
+
+      
     </div>
   );
 };
