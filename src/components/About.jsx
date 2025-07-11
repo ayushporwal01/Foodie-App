6dc67b5bd4e39050cb6688e69dada497c52b0eb8
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 11:33:50 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,24 @@
-const About = () => {
-  return (
-    <div className="h-screen flex justify-center items-center">
-      {/* Initial Content */}
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex items-center justify-center">
-          <h2 className="text-6xl max-w-1/2 font-bold text-[#555] leading-snug">
-            Home of your next favorite bite.
-          </h2>
+import { useState } from "react";
 
-          <img src="/pizza.png" className="w-96" alt="" />
-        </div>
-        <button className="px-4 py-2 bg-[#e08300] hover:bg-[#e08e00] text-md font-bold text-white rounded-full cursor-pointer">Show Profile</button>
+const About = () => {
+  const [showProfile, setShowProfile] = useState(false);
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      {/* Initial Content */}
+      <div className="flex items-center justify-center">
+        <h2 className="text-6xl max-w-1/2 font-bold text-[#555] leading-snug">
+          Home of your next favorite bite.
+        </h2>
+
+        <img src="/pizza.png" className="w-96" alt="" />
+      </div>
+
+      {/* Toggle Button */}
+      <div>
+        <button className="px-4 py-2 bg-[#e08300] hover:bg-[#e08e00] text-md font-bold text-white rounded-full cursor-pointer">
+          Show Profile
+        </button>
       </div>
     </div>
   );
