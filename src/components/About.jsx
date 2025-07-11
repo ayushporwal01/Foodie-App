7de5bbf8d282fd6f99fbf6656dbf222278bee550
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 10:22:28 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,14 @@
 const About = () => {
-
   return (
-    <div className="h-screen flex justify-center items-center overflow-y-hidden">
+    <div className="h-screen flex justify-center items-center">
       {/* Initial Content */}
       <div className="flex justify-center items-center gap-14">
-        <h2 className="text-4xl font-bold text-gray-700">
-          Home of your next favorite bite.
-        </h2>
+        <div c>
+          <h2 className="text-4xl font-bold text-gray-700">
+            Home of your next favorite bite.
+          </h2>
+          <button className="px-4 py-1.5 bg-black text-lg text-white font-bold rounded-md">Show Profile</button>
+        </div>
         <img src="/pizza.png" className="w-96" alt="" />
       </div>
     </div>
