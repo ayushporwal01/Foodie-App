34231797201fb195f640f0b56e93d9cc47164f7e
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 10:08:38 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,16 @@ import { useState } from "react";
 
 const About = () => {
   const [showProfile, setShowProfile] = useState(false);
-  
+
   return (
-    <div className="flex justify-center items-center py-48">
+    <div className="flex justify-center items-center py-44">
       {/* Initial Content */}
       <div className="flex justify-center items-center gap-14">
         <h2 className="text-4xl font-bold text-gray-700">
           Home of your next favorite bite."
         </h2>
-        <img src="/pizza.png" className="w-96 " alt="" />
+        <img src="/pizza.png" className="w-96" alt="" />
       </div>
-
-      {/* Show Profile Button */}
-      <button className="bg-blue-500">Show Profile</button>
     </div>
   );
 };
