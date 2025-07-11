48795693312258244e854dd02b28b12504e90de4
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 09:52:39 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,16 @@
+
+
 const About = () => {
   return (
-     <div>
-        <h1 className="text-4xl font-bold my-32 text-center text-gray-700 dark:text-white">About</h1>
-        <div className="flex justify-center items-center gap-5">
-          <h2 className="text-4xl font-bold text-gray-700">Home of your next favorite bite."</h2>
-          <img src="/pizza.png" className="w-96 " alt="" />
-        </div>
-     </div>
+    <div className="flex justify-center items-center py-48">
+      <div className="flex justify-center items-center gap-14">
+        <h2 className="text-4xl font-bold text-gray-700">
+          Home of your next favorite bite."
+        </h2>
+        <img src="/pizza.png" className="w-96 " alt="" />
+      </div>
+    </div>
   );
-}
+};
 
 export default About;
