98e245373131515792d528447a357a420ca0bd84
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 10:31:58 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,9 @@ const About = () => {
     <div className="h-screen flex justify-center items-center">
       {/* Initial Content */}
       <div className="flex justify-center items-center gap-14">
-        <div className="flex flex-col items-center gap-8">
-          <h2 className="text-4xl font-bold text-gray-600">
-            Home of your next favorite bite.
-          </h2>
-        </div>
+        <h2 className="text-7xl w-[58%] font-bold text-gray-600">
+          Home of your next favorite bite.
+        </h2>
         <img src="/pizza.png" className="w-96" alt="" />
       </div>
     </div>
