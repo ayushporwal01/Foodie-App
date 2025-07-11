4b71b3111254be942453bc546767f54cb907a309
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 10:27:12 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
       {/* Initial Content */}
       <div className="flex justify-center items-center gap-14">
         <div className="flex flex-col items-center gap-8">
-          <h2 className="text-4xl font-bold text-[#545454]">
+          <h2 className="text-4xl font-bold text-gray-600">
             Home of your next favorite bite.
           </h2>
         </div>
