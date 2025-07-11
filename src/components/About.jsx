f547bb0c6138ec569dfc9d812892165580cdf2ec
ayushporwal01 <porwalayush953@gmail.com>
2025-07-11 10:26:00 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
           <h2 className="text-4xl font-bold text-[#545454]">
             Home of your next favorite bite.
           </h2>
-          <button className="px-4 py-1.5 bg-black text-lg text-white font-bold rounded-md">Show Profile</button>
         </div>
         <img src="/pizza.png" className="w-96" alt="" />
       </div>
