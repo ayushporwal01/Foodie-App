1de42e5d9c4dc17c49d5357c228a17852dc04845
ayushporwal01 <porwalayush953@gmail.com>
2025-07-12 08:48:11 +0530

    episode-13

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,26 @@
+const Shimmer = () => {
+  return (
+    <div className="flex justify-center items-center dark:bg-black min-h-screen">
+      <div className="shimmer-container flex flex-wrap justify-center gap-10 py-4">
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+         <div className="w-56 h-64 bg-gray-300 animate-pulse"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmer;
