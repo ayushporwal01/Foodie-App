fb9af65c307aba5634b5f4e9375ca9e192ed8d5a
ayushporwal01 <porwalayush953@gmail.com>
2025-07-12 18:41:10 +0530

    episode-13

--- src/components/MenuShimmer.jsx
+++ src/components/MenuShimmer.jsx
@@ -1,9 +1,30 @@
 const MenuShimmer = () => {
-    return (
-        <div>
-            Menu Shimmer
+  return (
+    <div className="min-h-screen flex flex-col items-center">
+      {/* Header */}
+      <div className="w-[55%] h-50 bg-gray-200 flex justify-start items-center p-4 gap-x-5">
+        {/* Image */}
+        <div className="w-64 h-40 border border-[#adadad] rounded-sm"></div>
+        {/* Lines */}
+        <div className="flex flex-col flex-1 space-y-8">
+          <div className="w-[90%] h-4 bg-[#adadad]"></div>
+          <div className="w-[25%] h-4 bg-[#adadad] mt-2"></div>
+          <div className="flex space-x-5 mt-2">
+            <div className="w-[40%] h-4 bg-[#adadad]"></div>
+            <div className="w-[40%] h-4 bg-[#adadad]"></div>
+            <div className="w-[40%] h-4 bg-[#adadad]"></div>
+          </div>
         </div>
-    )
-}
+      </div>
+
+      {/* Menu */}
+      <div className="w-[55%] h-50 flex flex-col justify-start pt-16">
+        <div className="w-48 h-4 bg-[#adadad]">
+        
+      </div>    
+      </div>
+    </div>
+  );
+};
 
 export default MenuShimmer;
--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -57,11 +57,13 @@ const RestaurantMenu = () => {
     <div className="min-h-screen text-center pt-20 dark:bg-black">
       {/* Menu Header */}
       <div className="w-[55%] bg-[#171717] flex mx-auto items-center gap-x-5 p-5">
+        {/* Menu Image */}
         <img
           className="res-logo w-64 h-40 object-cover rounded-md"
           src={CDN_URL + cloudinaryImageId}
         />
-
+        
+        {/* Right Part */}
         <div className="flex flex-col justify-start items-start">
           <h1 className="text-4xl text-[#dbdbdb]">{name}</h1>
           <p className="text-sm text-[#b1b1b1] py-2">{cuisines?.join(", ")}</p>
