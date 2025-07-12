7c9b6ca9bb523ac40029e4800b121b19e7ad45cc
ayushporwal01 <porwalayush953@gmail.com>
2025-07-12 12:10:17 +0530

    episode-13

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,10 +2,16 @@ const Shimmer = () => {
   return (
     <div className="flex justify-center items-center dark:bg-black min-h-screen">
       <div className="flex flex-wrap justify-center gap-10 py-4">
-        <div className="w-60 p-4 bg-white rounded-lg shadow-2xl hover:shadow-black/50 transition-all duration-300 animate-pulse">
-            <div className="h-40 bg-gray-300 rounded mb-4"></div>
-            <div className="h-4 bg-gray-300 rounded w-3/4 mb-4"></div>
-            <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+        {/* Card */}
+        <div className="shimmer-card">
+          <div className="shimmer-img"></div>
+          <div className="shimmer-line"></div>
+          <div className="shimmer-line-short"></div>
+          <div className="shimmer-tags">
+            <div className="shimmer-tag"></div>
+            <div className="shimmer-tag"></div>
+            <div className="shimmer-tag"></div>
+          </div>
         </div>
       </div>
     </div>
