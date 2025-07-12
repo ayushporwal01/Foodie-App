6be57103905e236c1848fb43ff7fe959106901b2
ayushporwal01 <porwalayush953@gmail.com>
2025-07-12 21:23:24 +0530

    episode-13

--- src/components/MenuShimmer.jsx
+++ src/components/MenuShimmer.jsx
@@ -4,15 +4,15 @@ const MenuShimmer = () => {
       {/* Header */}
       <div className="w-[55%] h-50 bg-gray-200 flex justify-start items-center p-4 gap-x-5">
         {/* Image */}
-        <div className="w-64 h-40 border border-[#adadad] rounded-sm"></div>
+        <div className="w-64 h-40 border border-[#adadad] rounded-sm animate-shimmer"></div>
         {/* Lines */}
         <div className="flex flex-col flex-1 space-y-8">
-          <div className="w-[90%] h-4 bg-[#adadad]"></div>
-          <div className="w-[25%] h-4 bg-[#adadad] mt-2"></div>
+          <div className="w-[90%] h-4 bg-[#adadad] animate-shimmer"></div>
+          <div className="w-[25%] h-4 bg-[#adadad] mt-2 animate-shimmer"></div>
           <div className="flex space-x-5 mt-2">
-            <div className="w-[40%] h-4 bg-[#adadad]"></div>
-            <div className="w-[40%] h-4 bg-[#adadad]"></div>
-            <div className="w-[40%] h-4 bg-[#adadad]"></div>
+            <div className="w-[40%] h-4 bg-[#adadad] animate-shimmer"></div>
+            <div className="w-[40%] h-4 bg-[#adadad] animate-shimmer"></div>
+            <div className="w-[40%] h-4 bg-[#adadad] animate-shimmer"></div>
           </div>
         </div>
       </div>
@@ -21,106 +21,90 @@ const MenuShimmer = () => {
       <div className="w-[55%] h-50 flex flex-col justify-start pt-10">
         {/* Category */}
         <div className="ml-10 flex flex-col space-y-5">
-          <div className="w-48 h-4 bg-[#adadad]"></div>
-          <div className="w-30 h-4 bg-[#adadad]"></div>
+          <div className="w-48 h-4 bg-[#adadad] animate-shimmer"></div>
+          <div className="w-30 h-4 bg-[#adadad] animate-shimmer"></div>
         </div>
 
         {/* Items */}
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
         <div className="item-card">
           <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
+            <div className="item-line-md animate-shimmer"></div>
+            <div className="item-line-sm animate-shimmer"></div>
+            <div className="item-line-lg animate-shimmer"></div>
           </div>
-          <div className="item-image"></div>
-        </div>
-        <div className="item-card">
-          <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
-          </div>
-          <div className="item-image"></div>
-        </div>
-        <div className="item-card">
-          <div className="item-text-block">
-            <div className="item-line-md"></div>
-            <div className="item-line-sm"></div>
-            <div className="item-line-lg"></div>
-          </div>
-          <div className="item-image"></div>
+          <div className="item-image animate-shimmer"></div>
         </div>
       </div>
     </div>
