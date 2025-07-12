9120b68a980995bad09385a55434d851d0553fc3
ayushporwal01 <porwalayush953@gmail.com>
2025-07-12 10:57:08 +0530

    episode-13

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,7 +1,7 @@
 const Shimmer = () => {
   return (
     <div className="flex justify-center items-center dark:bg-black min-h-screen">
-      <div className="shimmer-container flex flex-wrap justify-center gap-10 py-4">
+      <div className="flex flex-wrap justify-center gap-10 py-4">
         <div className="w-60 p-4 bg-white rounded-lg shadow-2xl hover:shadow-black/50 transition-all duration-300 animate-pulse">
             <div className="h-40 bg-gray-300 rounded mb-4"></div>
             <div className="h-4 bg-gray-300 rounded w-3/4 mb-4"></div>
