6694adfd5460979055daca0ac531ce8f5b86685b
ayushporwal01 <porwalayush953@gmail.com>
2025-07-13 22:13:24 +0530

    episode-13

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,13 +1,24 @@
 const ShimmerCard = () => (
-
-)
+  <div className="shimmer-card">
+    <div className="shimmer-img animate-shimmer"></div>
+    <div className="shimmer-line animate-shimmer"></div>
+    <div className="shimmer-line-short animate-shimmer"></div>
+    <div className="shimmer-tags">
+      <div className="shimmer-tag animate-shimmer"></div>
+      <div className="shimmer-tag animate-shimmer"></div>
+      <div className="shimmer-tag animate-shimmer"></div>
+    </div>
+  </div>
+);
 
 const Shimmer = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-wrap justify-center gap-10 py-4">
         {/* Card */}
-       
+        {[...Array(20)].map((_, i) => (
+          <ShimmerCard key={i} />
+        ))}
       </div>
     </div>
   );
