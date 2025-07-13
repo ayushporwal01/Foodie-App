50bbb1a5fd0ded2347b5644ac6797b75e8525048
ayushporwal01 <porwalayush953@gmail.com>
2025-07-13 08:02:33 +0530

    episode-13

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
           Home of your next favorite meal.
         </h2>
 
-        <img src="/pizza.png" className="w-96" loading="lazy" alt="" />
+        <img src="/pizza.png" className="w-96" loading="lazy" alt="Pizza Image" />
       </div>
 
       {/* Profile Section */}
