411f643eed32ab1ee97bf1d548f900a3a8aa8d68
ayushporwal01 <porwalayush953@gmail.com>
2025-07-15 14:12:12 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    target: "defaults",
+    target: "esnext",
   },
 });
