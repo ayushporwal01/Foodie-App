2c7e48c20ae55e7b251e06784bfbd74b737d8423
ayushporwal01 <porwalayush953@gmail.com>
2025-07-16 12:41:32 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import BottomHeader from "./components/BottomHeader";
 import CartSync from "./utils/CartSync";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function AppLayout() {
   return (
     <Provider store={appStore}>
       <div className="app">
         <Header />
+        <ScrollToTop />
         <main>
           <Outlet />
         </main>
